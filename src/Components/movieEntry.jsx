0f442c5d90be63334338e040d97c5fa2dd730be1
chanychi <chan.ychi@gmail.com>
2022-02-14 16:48:03 -0600

    Added comment on conditional rendering

--- src/Components/movieEntry.jsx
+++ src/Components/movieEntry.jsx
@@ -23,6 +23,31 @@ const MovieEntry = (props) => {
     </div>
   )
 
+    //conditional rendering
+    
+  // if(props.singleMovie.Actors) {
+  //   return (
+  //     <div className="Movie">
+  //     <h1>{props.singleMovie.Title}</h1>
+  //     <span>Actors: {props.singleMovie.Actors}</span>
+  //     <p>Release Date: {props.singleMovie.Released}</p>
+  //     <p>Nominations: {props.singleMovie.Awards}</p>
+  //     <p>
+  //       <img src={props.singleMovie.Poster}
+  //         alt="movie poster"
+  //         onClick={() => {handleClick(props.singleMovie.Poster)}}
+  //         style={{cursor:'pointer'}}>
+  //       </img>
+  //     </p>
+  //   </div>
+  //   )
+  // } else {
+  //   return (
+  //     <div></div>
+  //   )
+  // }
+
+
 }
 
 export default MovieEntry;
