c051a27016cfb81170950456fe42b469ab696bd6
chanychi <chan.ychi@gmail.com>
2022-02-14 11:28:31 -0600

    Added open window and arrow hover

--- src/Components/movieEntry.jsx
+++ src/Components/movieEntry.jsx
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
+
 const MovieEntry = (props) => {
 
+  const handleClick = (link) => {
+    window.open(`${link}`);
+  };
+
   return (
-    <div>
-      <h1>{props.singleMovie?.Title}</h1>
-      <span>Actors: {props.singleMovie?.Actors}</span>
-      <p>Release Date: {props.singleMovie?.Released}</p>
-      <p><img src={props.singleMovie?.Poster} alt="movie poster"></img></p>
+    <div className="Movie">
+      <h1>{props.singleMovie.Title}</h1>
+      <span>Actors: {props.singleMovie.Actors}</span>
+      <p>Release Date: {props.singleMovie.Released}</p>
+      <p><img src={props.singleMovie.Poster}
+        alt="movie poster"
+        onClick={() => {handleClick(props.singleMovie.Poster)}}
+        style={{cursor:'pointer'}}>
+          </img>
+      </p>
     </div>
   )
 
